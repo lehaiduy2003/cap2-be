9630d819bfbfa59377ca8bca27ba14612fe11640
Le Hai Duy <lehaiduy2003@gmail.com>
2025-09-21 20:04:26 +0700

    development: fix build error

--- src/test/java/com/c1se_01/roomiego/RoomieGOApplicationTests.java
+++ src/test/java/com/c1se_01/roomiego/RoomieGOApplicationTests.java
@@ -2,8 +2,10 @@ package com.c1se_01.roomiego;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RoomieGOApplicationTests {
 
 	@Test
